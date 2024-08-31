467686fc839a53e622ac156e1d337600e9e0d96c
kaikaci12 <somewonesomewone@gmail.com>
2024-08-31 19:41:52 +0400

    handle post request for users execrcises, first validate the request body and then send back the validated data

--- index.js
+++ index.js
@@ -27,41 +27,41 @@ app.post("/api/users", async (req, res) => {
     username: username,
   });
 });
-// app.post(
-//   "/api/users/:_id/exercises",
-//   [
-//     body("description").notEmpty().withMessage("description cannot be empty"),
-//     body("duration")
-//       .notEmpty()
-//       .withMessage("duration is required")
-//       .isNumeric()
-//       .withMessage("duration must be a number"),
-//     body("date")
-//       .notEmpty()
-//       .withMessage("date cannot be empty")
-//       .isDate()
-//       .withMessage("Invalid date format"),
-//   ],
-//   async (req, res) => {
-//     const userId = req.params._id;
-//     const result = validationResult(req);
-//     if (!result.isEmpty()) {
-//       return res.status(400).send({ errors: result.array() });
-//     }
-//     const data = matchedData(req);
+app.post(
+  "/api/users/:_id/exercises",
+  [
+    body("description").notEmpty().withMessage("description cannot be empty"),
+    body("duration")
+      .notEmpty()
+      .withMessage("duration is required")
+      .isNumeric()
+      .withMessage("duration must be a number"),
+    body("date")
+      .notEmpty()
+      .withMessage("date cannot be empty")
+      .isDate()
+      .withMessage("Invalid date format"),
+  ],
+  async (req, res) => {
+    const userId = req.params._id;
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).send({ errors: result.array() });
+    }
+    const data = matchedData(req);
 
-//     console.log(data);
-//     res.send(data);
+    console.log(data);
+    res.send(data);
 
-//     const { description, duration, date } = req.body;
-//     const findUser = await User.findById(userId);
-//     if (!findUser) {
-//       return res
-//         .status(404)
-//         .send(`Failed to Add excersise to the user with the id of ${userId}`);
-//     }
-//   }
-// );
+    const { description, duration, date } = req.body;
+    const findUser = await User.findById(userId);
+    if (!findUser) {
+      return res
+        .status(404)
+        .send(`Failed to Add excersise to the user with the id of ${userId}`);
+    }
+  }
+);
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
